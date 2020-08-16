5a4df7efa72df59ef02b833df72416e090b6673f
shishirkanthi <shishirkanthi@gmail.com>
2020-08-16 16:19:44 +0530

    Updates

---
+++ precache-manifest.06912c3370384106301445d168db7c72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5192c32505fcaa3bce6f6e5daecb71",
+    "url": "/basicReact/index.html"
+  },
+  {
+    "revision": "0134cd3d1223adcb6c67",
+    "url": "/basicReact/static/css/main.f0c4e392.chunk.css"
+  },
+  {
+    "revision": "a82f1861f73b63d0eb18",
+    "url": "/basicReact/static/js/2.ef39f8d0.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/basicReact/static/js/2.ef39f8d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0134cd3d1223adcb6c67",
+    "url": "/basicReact/static/js/main.faa26443.chunk.js"
+  },
+  {
+    "revision": "1e332a14be81860ed093",
+    "url": "/basicReact/static/js/runtime-main.52debec6.js"
+  }
+]);
